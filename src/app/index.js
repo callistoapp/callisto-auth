9c8d1b8a63d8ef58e206f05cfdb1a4d414713124
cmondion <clement.mondion@clicrdv.com>
2018-03-31 16:37:54 +0200

    app index updated with new changes

--- src/app/index.js
+++ src/app/index.js
@@ -1,89 +1,87 @@
-import path from 'path';
-import express from 'express';
-import exphbs from 'express-handlebars';
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const auth = require("./authentication/jwt.js")();
-const jwt = require("jwt-simple");
-const UserModel = require('./models/user')
-const db = new UserModel()
-const session = require('express-session')
-const RedisStore = require('connect-redis')(session)
-const morgan = require('morgan');
-const config = require('../config')
-const app = express()
+import path from 'path'
+import express from 'express'
+import exphbs from 'express-handlebars'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+import jwt from 'jwt-simple'
 
-app.use(morgan('dev'));
+import initJwt from './auth/jwt'
+import UserModel from './models/user'
+import {jwtConfig} from '../config/index'
+import * as _ from "lodash";
+
+const app = express()
+const db  = new UserModel()
+
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-require('./authentication').init(app)
-app.use(auth.initialize());
+const {initialize, authenticate} = initJwt()
 
-app.use(session({
-  store: new RedisStore({
-    host: config.redisStore.host
-  }),
-  secret: config.redisStore.secret,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    path: "/",
-    domain: ".callisto.com"
-  }
-}))
-
-app.use(function (req, res, next) {
-  if (!req.session) {
-    console.log("No session")
-    return next(new Error('oh no')) // handle error
-  }
-  next() // otherwise continue
-})
-
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(initialize())
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'layout',
-  extname: '.hbs',
-  layoutsDir: path.join(__dirname),
-  partialsDir: path.join(__dirname)
+  extname      : '.hbs',
+  layoutsDir   : path.join(__dirname),
+  partialsDir  : path.join(__dirname)
 }))
 
-app.get("/user", auth.authenticate(), function(req, res) {
-  res.json(db.getUserById(req.user.id));
-});
-
-app.post("/token", function(req, res) {
-  console.log("Post token:", req.body)
+app.post("/login", function (req, res) {
   if (req.body.username && req.body.password) {
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log("Verifying user :", username, password)
-    db.verifyUser(username, password, user => {
-      if (user) {
+    const username = req.body.username
+    const password = req.body.password
+    db.verifyUser(username, password, (err, user) => {
+      console.log(user, err)
+      if (err === null && !_.isNil(user)) {
         const payload = {
           id: user.id
-        };
-        const token = jwt.encode(payload, config.jwt.jwtSecret);
+        }
+        const token   = jwt.encode(payload, jwtConfig.jwtSecret)
         res.json({
           token: token
-        });
+        })
       } else {
-        res.sendStatus(401);
+        res.sendStatus(401)
       }
-    });
+    })
   } else {
-    res.sendStatus(401);
+    res.sendStatus(401)
   }
-});
+})
 
+app.post("/register", function (req, res) {
+  if (req.body.username && req.body.email && req.body.password) {
+    const {username, email, password} = req.body
+    db.createUser({username, password, email}, (err, user) => {
+      if (err === null && !_.isNil(user)) {
+        const payload = {
+          id: user.id
+        }
+        const token   = jwt.encode(payload, jwtConfig.jwtSecret)
+        res.json({
+          token: token
+        })
+      } else {
+        res.sendStatus(401)
+      }
+    })
+  } else {
+    res.sendStatus(401)
+  }
+})
 
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname))
 
-require('./user').init(app)
+app.get("/check", authenticate('jwt', {session: false}), function (req, res) {
+  res.json("Success! Youre connected")
+})
 
-module.exports = app
+app.get("/", (req, res) => {
+  res.json("Welcome to callisto auth")
+})
+
+export default app
