4a4075bcb69fa3c3a6e0152fe99f8523641b9925
cmondion <clement.mondion@clicrdv.com>
2018-03-31 16:36:43 +0200

    feat(user model): cleaner way to handle users

--- initdb/auth.sql
+++ initdb/auth.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
   id INTEGER AUTO_INCREMENT,
   username VARCHAR(255) UNIQUE,
   password VARCHAR(255),
-  email VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
   verified_email TINYINT(1),
   PRIMARY KEY (id)
 );
--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,76 +1,82 @@
-const bcrypt = require("bcrypt");
-const mysql = require("../db/index");
-const randomstring = require("randomstring");
+import bcrypt from 'bcrypt'
+import mysql from '../db/index'
+import randomstring from 'randomstring'
 
 class User {
   constructor() {
-    this.getUser = this.getUser.bind(this);
-    this.verifyUser = this.verifyUser.bind(this);
-    this.getUserById = this.getUserById.bind(this);
-    this.createUser = this.createUser.bind(this);
-    this.checkPassword = this.checkPassword.bind(this);
-    this.generatePassword = this.generatePassword.bind(this);
+    this.getUser       = this.getUser.bind(this)
+    this.verifyUser    = this.verifyUser.bind(this)
+    this.getUserById   = this.getUserById.bind(this)
+    this.createUser    = this.createUser.bind(this)
+    this.checkPassword = this.checkPassword.bind(this)
+    this.hashPassword  = this.hashPassword.bind(this)
   }
 
-  getUser(username, cb) {
-    const query = `SELECT * FROM users WHERE username="${username}"`
-    mysql.query(query, (err,rows) => {
-      if(err) cb(err, null);
+  getUser(login, cb) {
+    const query = `SELECT * FROM users WHERE username="${login}" OR email="${login}"`
+    mysql.query(query, (err, rows) => {
+      if (err) cb(err, null)
 
-      const result = JSON.parse(JSON.stringify(rows));
+      const result = JSON.parse(JSON.stringify(rows))
 
       cb(null, result[0])
-    });
+    })
   }
 
   verifyUser(username, password, cb) {
     const query = `SELECT * FROM users WHERE username="${username}"`
-    mysql.query(query, (err,rows) => {
-      if(err) cb(err, false);
-
-      const result = JSON.parse(JSON.stringify(rows));
-
-      if (this.checkPassword(result[0].password, password))
-        cb(null, result[0])
-      cb(new Error("Unauthorized"), null)
-    });
-  }
-
-  getUserById(id, cb) {
-    const query = `SELECT * FROM users WHERE id="${id}"`
-    mysql.query(query, (err,rows) => {
-      if(err) cb(err, null);
-      const result = JSON.parse(JSON.stringify(rows));
-      cb(null, result[0])
-    });
-  }
-
-  createUser(username, cb) {
-    const password = this.generatePassword();
-    const query = `INSERT INTO users (username, password) VALUES ("${username}", "${password}")`
-    console.log(query);
     mysql.query(query, (err, rows) => {
-      if(err) cb(err, null);
-      this.getUser(username, cb)
-    });
-  }
-
-  checkPassword(stored, entered) {
-    bcrypt.compare(entered, stored, (err, isValid) => {
       if (err) {
-        console.log(err)
-        return false
+        cb(err, false)
+        return
       }
-      return isValid
+
+      const result = JSON.parse(JSON.stringify(rows))
+      this.checkPassword(result[0].password, password, valid => {
+        if (valid)
+          cb(null, result[0])
+        else
+          cb(new Error("Unauthorized"), null)
+      })
     })
   }
 
-  generatePassword() {
+  getUserById(id, cb) {
+    console.log('Getting user at id : ', id)
+    const query = `SELECT * FROM users WHERE id="${id}"`
+    mysql.query(query, (err, rows) => {
+      if (err) cb(err, null)
+      else {
+        const result = JSON.parse(JSON.stringify(rows))
+        cb(null, result[0])
+      }
+    })
+  }
+
+  createUser(user, cb) {
+    const pass  = this.hashPassword(user.password)
+    const query = `INSERT INTO users (username, password, email) VALUES ("${user.username}", "${pass}", "${user.email}")`
+    mysql.query(query, err => {
+      if (err) cb(err, null)
+      else this.getUser(user.username, cb)
+    })
+  }
+
+  checkPassword(stored, entered, cb) {
+    console.log('verifying pass', stored, entered)
+    bcrypt.compare(entered, stored, (err, isValid) => {
+      if (err) {
+        cb(false)
+      }
+      cb(isValid)
+    })
+  }
+
+  hashPassword(password) {
     const saltRounds = 10
-    const plaintextPassword = randomstring.generate(32)
-    const salt = bcrypt.genSaltSync(saltRounds)
-    return bcrypt.hashSync(plaintextPassword, salt)
+    const salt       = bcrypt.genSaltSync(saltRounds)
+    return bcrypt.hashSync(password, salt)
   }
 }
 
-module.exports = User;
+module.exports = User
