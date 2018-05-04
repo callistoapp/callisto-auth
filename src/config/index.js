1b6427269f86c29c13dc6e07405e1a9908c6cb07
cmondion <clement.mondion@clicrdv.com>
2018-05-04 12:19:54 +0200

    feat(config file): add google & github configs

--- src/config/index.js
+++ src/config/index.js
@@ -13,3 +13,13 @@ export const jwtConfig = {
     session: false
   }
 }
+
+export const githubConfig = {
+  CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+}
+
+export const googleConfig = {
+  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+}
