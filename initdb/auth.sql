6156547aa30409a5b97eb06df18f4ca99c21617d
cmondion <clement.mondion@clicrdv.com>
2018-05-04 12:17:57 +0200

    feat(bdd): add google & github ids

--- initdb/auth.sql
+++ initdb/auth.sql
@@ -5,6 +5,8 @@ CREATE TABLE users (
   username VARCHAR(255) UNIQUE,
   password VARCHAR(255),
   email VARCHAR(255) UNIQUE,
+  githubId INTEGER UNIQUE,
+  googleId VARCHAR(255) UNIQUE,
   verified_email TINYINT(1),
   PRIMARY KEY (id)
 );
