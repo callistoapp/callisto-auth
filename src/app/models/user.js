a7a529fcc041cef604e627aa163ae788c47c18db
cmondion <clement.mondion@clicrdv.com>
2018-05-04 12:24:17 +0200

    chore(user model): well formatted and dynamic user model

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,69 +1,72 @@
 import bcrypt from 'bcrypt'
 import mysql from '../db/index'
-import randomstring from 'randomstring'
+import * as _ from "lodash";
 
 class User {
   constructor() {
     this.getUser       = this.getUser.bind(this)
     this.verifyUser    = this.verifyUser.bind(this)
-    this.getUserById   = this.getUserById.bind(this)
     this.createUser    = this.createUser.bind(this)
     this.checkPassword = this.checkPassword.bind(this)
     this.hashPassword  = this.hashPassword.bind(this)
   }
 
-  getUser(login, cb) {
-    const query = `SELECT * FROM users WHERE username="${login}" OR email="${login}"`
+  findOrCreate(identity, cb) {
+    this.getUser(identity, (err, user) => {
+      if (err !== null) {
+        cb(err, null)
+        return
+      }
+
+      if (!_.isNil(user)) {
+        cb(null, user)
+      } else {
+        this.createUser(identity, cb)
+      }
+    })
+  }
+
+  getUser(identity, cb) {
+    const where = _.map(identity, (k, u) => {
+      return `${u}='${k}'`
+    }).join(' OR ')
+
+    const query = `SELECT * FROM users WHERE ${where}`
     mysql.query(query, (err, rows) => {
       if (err) cb(err, null)
-
       const result = JSON.parse(JSON.stringify(rows))
-
       cb(null, result[0])
     })
   }
 
-  verifyUser(username, password, cb) {
-    const query = `SELECT * FROM users WHERE username="${username}"`
-    mysql.query(query, (err, rows) => {
-      if (err) {
-        cb(err, false)
+  verifyUser(user, cb) {
+    this.getUser(_.omit(user, 'password'), (err, u) => {
+      if (err || _.isNil(u)) {
+        cb(err, null)
         return
       }
-
-      const result = JSON.parse(JSON.stringify(rows))
-      this.checkPassword(result[0].password, password, valid => {
+      this.checkPassword(u.password, user.password, valid => {
         if (valid)
-          cb(null, result[0])
+          cb(null, u)
         else
           cb(new Error("Unauthorized"), null)
       })
     })
   }
 
-  getUserById(id, cb) {
-    console.log('Getting user at id : ', id)
-    const query = `SELECT * FROM users WHERE id="${id}"`
-    mysql.query(query, (err, rows) => {
-      if (err) cb(err, null)
-      else {
-        const result = JSON.parse(JSON.stringify(rows))
-        cb(null, result[0])
-      }
-    })
-  }
-
   createUser(user, cb) {
-    const pass  = this.hashPassword(user.password)
-    const query = `INSERT INTO users (username, password, email) VALUES ("${user.username}", "${pass}", "${user.email}")`
+    if (user.hasOwnProperty('password'))
+      user.password = this.hashPassword(user.password)
+    const keys = _.keys(user).join()
+    const values = _.map(_.values(user), u => `"${u}"`).join()
+    const query = `INSERT INTO users (${keys}) VALUES (${values})`
     mysql.query(query, err => {
       if (err) cb(err, null)
-      else this.getUser(user.username, cb)
+      else this.getUser({username: user.username}, cb)
     })
   }
 
   checkPassword(stored, entered, cb) {
-    console.log('verifying pass', stored, entered)
     bcrypt.compare(entered, stored, (err, isValid) => {
       if (err) {
         cb(false)
